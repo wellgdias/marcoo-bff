2075331a811ea0f31b462e5f965f185352e39bd1
Wellington Dias <wads90@gmail.com>
2021-06-11 17:28:52 -0300

    fix: address endpoint

--- src/business/rules/get-address.ts
+++ src/business/rules/get-address.ts
@@ -3,7 +3,7 @@ import { NotFoundError } from '../errors';
 
 export default async function getAddress(cep: string, token: string, addressUrl: string) {
   const address = await getAddressSerice(cep, token, addressUrl);
-  if (!address) {
+  if (!address.cep) {
     throw new NotFoundError('CEP não encontrado');
   }
   return address;
